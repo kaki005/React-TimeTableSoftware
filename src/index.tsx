dc912dac3c510cf22ea138c4bb6c5d8a41860b74
kaki005 <u097498f@ecs.osaka-u.ac.jp>
2022-03-26 02:06:10 +0900

    Add HomePage

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,18 @@
 import ReactDOM from 'react-dom';
+import React from "react";
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 import TimeTable from "./component/TimeTable";
 import App from './App';
-
+import { BrowserRouter as Router } from 'react-router-dom';
 
 ReactDOM.render(
-    <App />,
-  document.getElementById('root')
+  <React.StrictMode>
+    <Router basename={process.env.PUBLIC_URL}>
+      <App />
+    </Router>
+  </React.StrictMode>,
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
